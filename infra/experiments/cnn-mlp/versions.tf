db72a4d36a49909324189e562964d8c9860acd36
atractorcrew <atractorcrew@gmail.com>
2025-09-13 12:23:52 -0600

    feature: Add the defenitions of the provider from GCP

---
+++ infra/experiments/cnn-mlp/versions.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    google = { source = "hashicorp/google", version = "~> 5.33" }
+  }
+  # backend "gcs" { ... }  # opcional si usas estado remoto
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
