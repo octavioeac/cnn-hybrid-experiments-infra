e30b109ebed21667dd181a274ef0e3f8172762c5
atractorcrew <atractorcrew@gmail.com>
2025-09-15 10:29:15 -0600

    fix: Run the  terraform fmt -recursive v4

--- infra/foundation/versions.tf
+++ infra/foundation/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     google = { source = "hashicorp/google", version = "~> 5.33" }
   }
-  backend "gcs" {}
+  #backend "gcs" {}
 }
 
 provider "google" {
--- infra/modules/experiment/versions.tf
+++ infra/modules/experiment/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     google = { source = "hashicorp/google", version = "~> 5.33" }
   }
-  # backend "gcs" { ... }  # opcional si usas estado remoto
+  backend "gcs" {} # opcional si usas estado remoto
 }
 
 provider "google" {
