0e8ae929311e45708e2e01c7d043a3158dd5e5ff
atractorcrew <atractorcrew@gmail.com>
2025-09-19 15:38:16 -0600

    fix: Move the pyproject.toml and change the path of module

--- apps/kaggle-upload/app/main.py
+++ apps/kaggle-upload/app/main.py
@@ -1,11 +1,17 @@
-from infrastructure.kaggle_service import KaggleService
-from infrastructure.gcs_storage import GCSStorageService
-from application.fetch_and_export import FetchAndExport
+from .infrastructure.kaggle_service import KaggleService
+from .infrastructure.gcs_storage import GCSStorageService
+from .application.fetch_and_export import FetchAndExport
 
-if __name__ == "__main__":
+
+def main() -> None :
     kaggle = KaggleService()
     gcs = GCSStorageService(bucket="my-test-bucket", prefix="datasets/")
     uc = FetchAndExport(kaggle, gcs)
 
     # prueba con datos ficticios
     uc.run("urbansound8k/urbansound8k", "./data", "us8k/")
+
+
+
+if __name__ == "__main__":
+    main()
